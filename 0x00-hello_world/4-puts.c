596cfaefe980c0037200c53358dde0ecee20aff1
Patrick Lolung <lolungpatrick18@gmail.com>
2023-02-19 00:43:46 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  * return: 0 (success)
 */
 
-int main (void){
-	puts("\"Programming is like building a multilingual puzzle"\n);
-
-	return 0;
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
