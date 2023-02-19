7efe4fa8a3f826c90730aea12ec58d08a315ff55
Patrick Lolung <lolungpatrick18@gmail.com>
2023-02-19 08:44:03 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,13 +2,8 @@
 
 /**
  * main - entry point
-<<<<<<< HEAD
  * Return:0 (success)
  */
-=======
- * Return:0 (successful)
-*/
->>>>>>> 4a7e4370adb326149f25ad114d412b481b4a27a8
 
 int main(void)
 {
@@ -17,19 +12,12 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
-<<<<<<< HEAD
-printf("size of a char: %lu byte(s)\n",(unsigned long)sizeof(a));
-printf("size of an int: %lu byte(s)\n",(unsigned long)sizeof(b));
-printf("size of long int: %lu byte(s)\n",(unsigned long)sizeof(c));
-printf("size of long long int: %lu byte(s)\n",(unsigned long)sizeof(d));
-printf("size of a float: %lu byte(s)\n",(unsigned long)sizeof(f));
-=======
 
-printf("size of a char:%lu byte(s)\n", (unsigned long)sizeof(a));
-printf("size of an int:%lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of a long int:%lu byte(s)\n", (unsigned long)sizeof(c));
-printf("size of a long long int:%lu byte(s)\n", (unsigned long)sizeof(d));
-printf("size of a float:%lu byte(s)\n", (unsigned long)sizeof(f));
->>>>>>> 4a7e4370adb326149f25ad114d412b481b4a27a8
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
 return (0);
 }
